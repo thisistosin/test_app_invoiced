58a1f3054bd652337ee4e4fbf188596f7329b2f6
Tosin <this.tosin@gmail.com>
2023-08-30 17:15:20 -0400

    cors and devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users #I'm not sure if I have seen this format
   namespace :v1 do
     resources :contacts
+    resource :sessions, only: [:create, :destroy]
   end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
